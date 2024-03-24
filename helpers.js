4b54203610327cd982488e03a87f4de41f3c08f2
David Leda <davmail99@yahoo.com>
2024-03-24 18:39:26 -0300

    1st commit

---
+++ helpers.js
@@ -0,0 +1,31 @@
+export async function getNewTab(browser) {
+
+  let resultPromise;
+
+  async function onTargetcreatedHandler(target) {
+      if (target.type() === 'page') {
+          const newPage = await target.page()
+          const newPagePromise = new Promise(y =>
+              newPage.once('domcontentloaded', () => y(newPage))
+          )
+
+          const isPageLoaded = await newPage.evaluate(
+              () => document.readyState
+          )
+
+          browser.off('targetcreated', onTargetcreatedHandler) // unsubscribing
+
+          return isPageLoaded.match('complete|interactive')
+              ? resultPromise(newPage)
+              : resultPromise(newPagePromise)
+      }
+  }
+
+  return new Promise(resolve => {
+      resultPromise = resolve
+      browser.on('targetcreated', onTargetcreatedHandler)
+  })
+}
+
+
+export const sleep = delay => new Promise((resolve) => setTimeout(resolve, delay))
---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+
+import puppeteer from 'puppeteer';
+import { getNewTab, sleep } from './helpers.js';
+
+const turnosPage = 'https://buenos-aires.diplo.de/ar-es/service/turnos/1801270?openAccordionId=item-2076388-0-panel'
+  
+const turnoElement = "#item-2076388-0-panel > div > p > a"
+
+// Abrir Chromium
+// Para poder visualizar > headleess: false 
+const browser = await puppeteer.launch({ headless: false })
+const page = await browser.newPage()
+
+// Ir a la pagina de turnos de la embajada
+await page.goto(turnosPage)
+
+// Cliquear en 'Reservar turno'
+await page.click(turnoElement)
+
+// Ir al nuevo tab del browser
+const newPage = await getNewTab(browser)
+
+await newPage.bringToFront()
+
+// Esperar 1 segundo y clickear
+sleep(1000)
+const continuarButton = '#content > div.wrapper > div:nth-child(13) > a'
+await newPage.click(continuarButton)
+
+// Esperar 1 segundo y clickear
+sleep(1000)
+const continuarButton2 = '#content > div.wrapper > div:nth-child(5) > a'
+await newPage.click(continuarButton2)
+
+// Esperar 1 segundo y clickear
+sleep(1000)
+const continuarButton3 = '#content > div.wrapper > h3:nth-child(2) > a:nth-child(2)'
+await newPage.click(continuarButton3)
+
+
+
